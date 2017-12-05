6eb1dfd87daf9802649fcc59ea72772d04bda72b
Nathaniel Edgar <n.edgar@ufl.edu>
2017-12-05 10:35:46 -0500

    Initial commit - added some test power pill seeker behavior

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -3,20 +3,40 @@ package ufl.cs1.controllers;
 import game.controllers.DefenderController;
 import game.models.Defender;
 import game.models.Game;
+import game.models.Node;
 
 import java.util.List;
 
 public final class StudentController implements DefenderController
 {
-	public void init(Game game) { }
+	private int currLevel;
+	private List<Node> powerPillLocs;
+
+	public void init(Game game)
+	{
+		currLevel = game.getLevel();
+		powerPillLocs = game.getPowerPillList();
+	}
 
 	public void shutdown(Game game) { }
 
 	public int[] update(Game game,long timeDue)
 	{
+		if (game.getLevel() != currLevel)
+		{
+			powerPillLocs = game.getPowerPillList();
+			currLevel = game.getLevel();
+		}
+
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
-		
+
+		for (int i = 0; i < 4; i++)
+		{
+			actions[i] = game.getDefender(i).getNextDir(powerPillLocs.get(i), true);
+		}
+		/*
+
 		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts
 		for(int i = 0; i < actions.length; i++)
@@ -28,6 +48,7 @@ public final class StudentController implements DefenderController
 			else
 				actions[i] = -1;
 		}
+		*/
 		return actions;
 	}
 }
